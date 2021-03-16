8f5adfa0d2b860364ae2a5e6638287cfffd958bc
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-03-17 08:09:59 +0900

    permmit audio and media

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const getMedia = async () => {
+  const constraints = {
+    audio: true,
+    video: true,
+  }
+
+  try {
+    await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+    console.log(err)
+  }
+}
+
+getMedia()
+
+const App = () => {
+  return (
+    <div>
+      Hello React
+    </div>
+  );
+}
+
+export default App;
